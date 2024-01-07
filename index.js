40a3b6ed4fcf6629e92f8c9f6f685b16229aed0d
Dipankar Ganguly <dipankarganguly15@gmail.com>
2024-01-08 00:17:56 +0530

    Adding the main files

---
+++ index.js
@@ -0,0 +1,38 @@
+var randNumber_1 = Math.floor((Math.random() * 6) + 1);
+var randNumber_2 = Math.floor((Math.random() * 6) + 1);
+
+if(randNumber_1 === 1) {
+    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+} else if(randNumber_1 === 2) {
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+} else if(randNumber_1 === 3) {
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+} else if(randNumber_1 === 4) {
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+} else if(randNumber_1 === 5) {
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+} else if(randNumber_1 === 6) {
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+}
+
+if(randNumber_2 === 1) {
+    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+} else if(randNumber_2 === 2) {
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+} else if(randNumber_2 === 3) {
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+} else if(randNumber_2 === 4) {
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+} else if(randNumber_2 === 5) {
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+} else if(randNumber_2 === 6) {
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+}
+
+if(randNumber_1 === randNumber_2) {
+    document.querySelector("h2").innerHTML = "Draw"
+} else if(randNumber_1 > randNumber_2) {
+    document.querySelector("h2").innerHTML = "Player 1 Won"
+} else if(randNumber_1 < randNumber_2) {
+    document.querySelector("h2").innerHTML = "Fuck Player 2 Won"
+}
